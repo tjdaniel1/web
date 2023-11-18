b2dd886b2dd908f017bf12ed044565d73ffd6897
tjdaniel1 <robot2daniel@gmail.com>
2023-11-18 18:26:40 +0900

    12

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req,res){
+router.get('/',function(req,res,next){
     res.render('index',{title:'게시판', pageName:'posts/list.ejs'})
 });
-router.get('/insert',function(req,res){
+router.get('/insert',function(req,res,next){
     res.render('index',{title:'글쓰기', pageName:'posts/insert.ejs'})
+});
+router.get('/read',function(req,res,next){
+    const id=req.query.id;
+    res.render('index',{title:'게시글정보', pageName:'posts/read.ejs',id})
 });
 
 
